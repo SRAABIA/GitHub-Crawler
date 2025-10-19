35051c11def744c223a524204813a635373642d1
Syeda Rabia <91334836+SRAABIA@users.noreply.github.com>
2025-10-20 02:05:57 +0500

    fixed error on union

--- infrastructure/github_client.py
+++ infrastructure/github_client.py
@@ -9,20 +9,23 @@ class GitHubClient:
 
     def fetch_repositories(self, query, after_cursor=None):
         gql = f"""
-        {{
-          search(query: "{query}", type: REPOSITORY, first: 100, after: {json.dumps(after_cursor) if after_cursor else "null"}) {{
-            pageInfo {{
-              endCursor
-              hasNextPage
-            }}
-            nodes {{
-              id
-              nameWithOwner
-              stargazerCount
-            }}
-          }}
-        }}
-        """
+              {{
+                search(query: "{query}", type: REPOSITORY, first: 100, after: {json.dumps(after_cursor) if after_cursor else "null"}) {{
+                  pageInfo {{
+                    endCursor
+                    hasNextPage
+                  }}
+                  nodes {{
+                    ... on Repository {{
+                      id
+                      nameWithOwner
+                      stargazerCount
+                    }}
+                  }}
+                }}
+              }}
+              """
+
         response = requests.post(self.url, json={"query": gql}, headers=self.headers)
         if response.status_code != 200:
             raise Exception(f"GitHub API failed: {response.status_code}")
